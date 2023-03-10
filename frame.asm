bd4273015908c84b4338beee6ae42dbee06db640
VictorBerbenets <112126887+VictorBerbenets@users.noreply.github.com>
2023-03-10 19:14:54 +0300

    Update frame.asm

--- frame.asm
+++ frame.asm
@@ -3,8 +3,6 @@
 
 org 100h
 
-
-
 Ramochka:
 
         Left_koords EQU ((25 - left_y)*80 + left_x)*2
@@ -29,14 +27,34 @@ Gorizont_up: ; up gorizontal line of the frame
 
 Verticals:  ;print vertical lines of the frame
 
+        mov al, 03h
+        mov ah, 00100100b
         add di, (80 - length_ram)*2 ; di points to the component of the right vertical line
 
         stosw
 
+        push cx
+        push di
+;;;;;;;;;;;;;Filling ram space;;;;;;;;;;;;;;;;
+        mov cx, length_ram - 2
+        mov al, 20h
+        mov ah, 00110000b
+        Fill_String:
+
+        stosw
+
+        loop Fill_String
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        pop di
+        pop cx
+
+        mov al, 03h
+        mov ah, 00100100b
+
         add di, (length_ram - 2)*2     ; di points to the component of the left  vertical line
 
         stosw
-        sub cx, 1
+        dec cx
 
         cmp cx, 0
         ja Verticals
@@ -44,6 +62,7 @@ Verticals:  ;print vertical lines of the frame
         std
         mov cx, length_ram - 1
         sub di, 2
+
 Print_bottom_line:
         
         stosw
@@ -55,19 +74,28 @@ Print_bottom_line:
 Print_Text:
 
         ; mov dx,  text
+        TextStart EQU (80*(25 - left_y + 2) + left_x + 10)*2
         mov ax, 0b800h
         mov es, ax
         xor ax, ax
         xor si, si
-	mov di, (80*12 + 30)*2
+	mov di, TextStart
+        xor bl, bl
         mov cx, str_size
 
         cld
         Cycle:
         
         mov al, [string + si]
-        mov ah, 00000100b
+        mov ah, 01110100b
+        inc bl
         stosw
+
+        cmp bl, 16
+        jne Skip
+                add di, (80 - 16)*2 
+                xor bl, bl
+        Skip:
         inc si
         loop Cycle
 
@@ -77,15 +105,13 @@ Print_Text:
 .data
 
 Max_y_size = 22
-left_x     = 10
-left_y     = 23
-width_ram  = 20
-length_ram = 54
+left_x     = 26
+left_y     = 20
+width_ram  = 10
+length_ram = 38
 
-;;;;;;;;;;;;;;;;;; Text ;;;;;;;;;;;;;;;;;;;;;;;
-string db 'Hello, world!!!'
+string db 'Hello world'
 str_size EQU $ - string
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 end Ramochka
 
